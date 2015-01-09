33e2e5f1ca367a9fd699b5a04c69c9dcffea27f9
Johan Van den Brande <johan@tektonica.com>
2015-01-09 18:28:04 +0100

    bumped to binutils 2.25

--- avr-binutils.rb
+++ avr-binutils.rb
@@ -3,16 +3,16 @@ require 'formula'
 class AvrBinutils < Formula
 
     homepage 'http://www.gnu.org/software/binutils/binutils.html'
-    url 'http://ftpmirror.gnu.org/binutils/binutils-2.24.tar.gz'
-    mirror 'http://ftp.gnu.org/gnu/binutils/binutils-2.24.tar.gz'
-    sha1 '1b2bc33003f4997d38fadaa276c1f0321329ec56'
+    url 'http://ftpmirror.gnu.org/binutils/binutils-2.25.tar.gz'
+    mirror 'http://ftp.gnu.org/gnu/binutils/binutils-2.25.tar.gz'
+    sha1 'f10c64e92d9c72ee428df3feaf349c4ecb2493bd'
 
     # Support for -C in avr-size. See issue
     # https://github.com/larsimmisch/homebrew-avr/issues/9
-    patch :p0 do
-        url 'https://gist.github.com/larsimmisch/4190960/raw/b36f3d6d086980006f097ae0acc80b3ada7bb7b1/avr-binutils-size.patch'
-        sha1 'b6d1ff7084b1f0a3fd2dee5383019ffb202e6c9a'
-    end
+    #patch :p0 do
+    #    url 'https://gist.github.com/larsimmisch/4190960/raw/b36f3d6d086980006f097ae0acc80b3ada7bb7b1/avr-binutils-size.patch'
+    #    sha1 'b6d1ff7084b1f0a3fd2dee5383019ffb202e6c9a'
+    #end
 
     def install
         args = [
