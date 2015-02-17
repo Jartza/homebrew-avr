a7c3caa529399db7ceee05bdd6fb3ee3592222fa
Jari Tulilahti <jari@rakettitiede.com>
2015-02-17 18:35:08 +0200

    Add version

--- avrdude-attiny841.rb
+++ avrdude-attiny841.rb
@@ -2,6 +2,7 @@ require 'formula'
 
 class AvrdudeAttiny841 < Formula
     url 'https://raw.githubusercontent.com/Jartza/homebrew-avr/master/patches/avrdude/avrduderc.tgz'
+    version '0.1'
     depends_on 'avrdude'
 
     def install
