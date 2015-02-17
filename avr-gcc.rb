4d4641c3e117cf8976e66e7b78a15f5040c546d8
Jari Tulilahti <jari@rakettitiede.com>
2015-02-17 17:54:37 +0200

    Test file...

--- avr-gcc.rb
+++ avr-gcc.rb
@@ -21,7 +21,7 @@ class AvrGcc < Formula
     option 'disable-cxx', 'Don\'t build the g++ compiler'
 
     patch do
-        url "patches/avr-gcc/tiny441-tiny841.patch"
+        url "file://patches/avr-gcc/tiny441-tiny841.patch"
         sha1 "eb669f806aa2cd43dec04ad11450337783a6da19"
     end
 
