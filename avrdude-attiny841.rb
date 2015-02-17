5bbdfcaf8740d1670418a7da1bce7d290763ef58
Jari Tulilahti <jari@rakettitiede.com>
2015-02-17 18:23:51 +0200

    Add dep

--- avrdude-attiny841.rb
+++ avrdude-attiny841.rb
@@ -1,6 +1,8 @@
 require 'formula'
 
 class AvrDudeAttiny841 < Formula
+    depends_on 'avrdude'
+
     def install
         system "https://raw.githubusercontent.com/Jartza/homebrew-avr/master/patches/avrdude/avrduderc >> ~/.avrduderc"
     end
