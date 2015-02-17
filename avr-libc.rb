b65911d93e08b52feca47c161b22426dcdce9973
Jari Tulilahti <jari@rakettitiede.com>
2015-02-17 22:04:44 +0200

    Patch the patched patch

--- avr-libc.rb
+++ avr-libc.rb
@@ -10,7 +10,7 @@ class AvrLibc < Formula
 
     patch do
         url "https://raw.githubusercontent.com/Jartza/homebrew-avr/master/patches/avr-libc/tiny441-tiny841.patch"
-        sha1 "d5e360861ba1e86b9e86e9bde8c0f9c008c2b8bf"
+        sha1 "91134ef9b905cbcfca2eb0b4aa845f6fa4a9912f"
     end
 
 
